f0c8142ce9856cb28fe9dc13302f0d1b2af6dde5
betrand1999 <132092468+Betrand1999@users.noreply.github.com>
2024-12-09 01:10:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="betrand_ngoh_mutagha_version_5",  # Updated to reflect your project name
-    version="0.1.5",
+    name="betrand_ngoh_mutagha_version_6",  # Updated to reflect your project name
+    version="0.1.6",
     description="A simple CI/CD Flask application",
     author="BETRAND MUTAGHA",  # Replace with your name if needed
     author_email="mutagha2@gmail.com",  # Replace with your email
