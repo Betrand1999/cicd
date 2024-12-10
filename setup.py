41e91079003648851b55d9713788c378c4d7852e
BETRAND <mmutagha@gmail.com>
2024-12-10 01:18:56 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="betrand_ngoh_mutagha_version_11",  # Updated to reflect your project name
-    version="0.1.23",
+    version="0.1.24",
     description="A simple CI/CD Flask application",
     author="BETRAND MUTAGHA",  # Replace with your name if needed
     author_email="mutagha2@gmail.com",  # Replace with your email
