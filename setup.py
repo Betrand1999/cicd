253c531ef3dfc2fa80d71a81d9d6887fb3884c5e
BETRAND <mmutagha@gmail.com>
2024-12-10 19:50:03 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="betrand_ngoh_mutagha_version_12",  # Updated to reflect your project name
-    version="0.1.31 simple CI/CD Flask application",
+    version="0.1.31",  # Valid version string as per PEP 440
     author="BETRAND MUTAGHA",  # Replace with your name if needed
     author_email="mutagha2@gmail.com",  # Replace with your email
     url="https://github.com/Betrand1999/cicd",  # Updated with your repo URL
@@ -19,4 +19,3 @@ setup(
         "Programming Language :: Python :: 3.10",
     ],
 )
-
